be1a8a79f350c998706fd83856a8180ba820e6ce
Régis GRARE <regisgrare@hotmail.fr>
2022-06-30 10:13:07 +0200

    fixed6

--- webpush.js
+++ webpush.js
@@ -8,7 +8,7 @@ const vapidKeys = {
 };
 
 webpush.setVapidDetails(
-  "mailto:example@yourdomain.org",
+  "https://musical-palmier-103be5.netlify.app/",
   vapidKeys.publicKey,
   vapidKeys.privateKey
 );
@@ -16,12 +16,12 @@ webpush.setVapidDetails(
 // This is the same output of calling JSON.stringify on a PushSubscription
 const pushSubscription = {
   endpoint:
-    "https://fcm.googleapis.com/fcm/send/drItUS6X_SA:APA91bFOLIfO82wNFsS3SmDxixnekcD7OHl5pIMgDwNVdZ11YGeQfgxT5PkCHhHxUy3sokVDwRNN1In1v_0_Hvh3WTXIkB61eXpVwK-gHSpqU0JYpkLYt_2Rft9O_D8cMk_inUmCzliz",
+    "https://fcm.googleapis.com/fcm/send/dbrMHcdeh78:APA91bHIYzSm2wmIiSS5BsJZ5eivhU-ldwiUvA4HNNbmJbtnhaE_a-x2TBXIJrXQfbv5_1Htu0nr8YCY0COdxNG8RluJ86lX8Db_NBHEQf8zs5AW6ncpsEQ2lS7ka8BBMyRm-FGKEwjM",
   expirationTime: null,
   keys: {
     p256dh:
-      "BPZyJgoIP9E9gTv-MZe6sQvcNiFmw_PpUhDeDc6fYTPv-F3vzA3A3MCmN8Cweg8B-oyP-yNAc0ifG69gLbmSxgg",
-    auth: "3o06Fgp-ogGQsUAT7Pwu3g",
+      "BNRyLfI2LZYM2z117wGoR8-5t6HH7Q7iYvhPIR8oT92UzQVrw19IvRIgTgVU1JUNotw1hcgN3nqsZM3TE11AJkA",
+    auth: "-N4OUfdHUIdE4N8aToKzFw",
   },
 };
 
