76a8b3453ca4919899e491cc56251e78998e88a0
Régis GRARE <regisgrare@hotmail.fr>
2022-06-30 09:15:29 +0200

    fxed

--- sw.js
+++ sw.js
@@ -1,3 +1,24 @@
-self.addEventListener('push', () => {
-  self.registration.showNotification('Hello world!', options);
+self.addEventListener("push", function (e) {
+  var options = {
+    body: "Push généré ! ",
+    icon: "images/example.png",
+    vibrate: [100, 50, 100],
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: "2",
+    },
+    actions: [
+      {
+        action: "explore",
+        title: "Explore this new world",
+        icon: image / xmark.png,
+      },
+      {
+        action: "close",
+        title: "Close",
+        icon: "images/xmark.png",
+      },
+    ],
+  };
+  e.waitUntil(self.registration.showNotification("Hello world!", options));
 });
--- webpush.js
+++ webpush.js
@@ -1,25 +1,28 @@
-const webpush = require('web-push');
+const webpush = require("web-push");
 
 // VAPID keys should only be generated only once.
 const vapidKeys = {
   publicKey:
-    'BBZY7Q3KEtZArAAWMLi_qzWHbH4vAoqPpIXnRhmlUaw0PVs1Kt_2fgLhuaVI5i8MWASBKx3d6W6UoH2U3qChw9U',
-  privateKey: 'CZtf_JUxmXkCKbzwaKedPPO9BFC99U2rk-GUYDbYAa8'
+    "BBZY7Q3KEtZArAAWMLi_qzWHbH4vAoqPpIXnRhmlUaw0PVs1Kt_2fgLhuaVI5i8MWASBKx3d6W6UoH2U3qChw9U",
+  privateKey: "CZtf_JUxmXkCKbzwaKedPPO9BFC99U2rk-GUYDbYAa8",
 };
 
 webpush.setVapidDetails(
-  'mailto:example@yourdomain.org',
+  "mailto:example@yourdomain.org",
   vapidKeys.publicKey,
   vapidKeys.privateKey
 );
 
 // This is the same output of calling JSON.stringify on a PushSubscription
 const pushSubscription = {
-  endpoint: '.....',
+  endpoint:
+    "https://fcm.googleapis.com/fcm/send/eZijYmQLkHk:APA91bE4qo7HotunmTXPwhRLGj9-oT6mNoJ3skQMje0JdY90Q08SV-z9gyi8rpTHorcIhht0Vutm5xWkYRjObKcypWO28F_GkB6T1PwrFPnuxPsQGbnejLA9IJxyuYVxCd6ztYdXeGAy",
+  expirationTime: null,
   keys: {
-    auth: '.....',
-    p256dh: '.....'
-  }
+    p256dh:
+      "BKU_jwCKQsN98yr86HOZntvMNZAw-Y4dZ4RCE4JGTzqzdqEXCRCNkUDYhtwxoWPBufjjzK-7bEzUrbVOuSeeXUE",
+    auth: "QdkDckGP7cxssMM73Q-Evw",
+  },
 };
 
-webpush.sendNotification(pushSubscription, 'Your Push Payload Text');
+webpush.sendNotification(pushSubscription, "Your Push Payload Text");
